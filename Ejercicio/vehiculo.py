9ba3ea1f08fc9c50bd28c32806b97ad4994f007f
Jose <josemanueldiazvilla@gmail.com>
2023-02-14 12:48:22 -0500

    clase vehiculo

--- Ejercicio/vehiculo.py
+++ Ejercicio/vehiculo.py
@@ -1,1 +1,5 @@
-a
+class Vehiculo:
+
+    def __init__(self, velocidad_maxima: float, kilometraje: int):
+        self.velocidad_maxima:float = velocidad_maxima
+        self.kilometraje: int = kilometraje
